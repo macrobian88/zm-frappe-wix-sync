a09b9e1ed5d9be3e12e72e83850b9db2934f004c
macrobian88 <ramshankarsc9@gmail.com>
2025-09-16 20:40:59 +0530

    Update setup.py with better error handling and Frappe compatibility

--- setup.py
+++ setup.py
@@ -4,16 +4,37 @@ with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in zm_frappe_wix_sync/__init__.py
-from zm_frappe_wix_sync import __version__ as version
+try:
+    from zm_frappe_wix_sync import __version__ as version
+except ImportError:
+    version = "0.0.1"
 
 setup(
     name="zm_frappe_wix_sync",
     version=version,
     description="A Frappe app to sync ERPNext items with Wix products",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="ZM Tech",
+    author_email="tech@zmtech.com",
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=install_requires
+    install_requires=install_requires,
+    python_requires=">=3.8",
+    license="MIT",
+    keywords=["frappe", "erpnext", "wix", "sync", "ecommerce"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers", 
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    url="https://github.com/macrobian88/zm-frappe-wix-sync",
+    project_urls={
+        "Bug Reports": "https://github.com/macrobian88/zm-frappe-wix-sync/issues",
+        "Source": "https://github.com/macrobian88/zm-frappe-wix-sync",
+    },
 )
